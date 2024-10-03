ca52dbd23bc899fbbfde368a4f5623a3572cfa66
Hung Nguyen <hungnguyen.cosc@gmail.com>
2024-10-03 09:54:04 +0700

    #feat: generate qr code by text

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import telebot
 import os
+from utils.qrcode import generate_qr_code
+
 # * This app receives text and sends qr code as a response
 # Load environment variables from the .env file
 load_dotenv()
@@ -20,5 +22,9 @@ def send_help(message):
     bot.reply_to(
         message, "I can generate QR codes for you. Just send me the text you want to convert to QR code.")
 
+@bot.message_handler(func=lambda message: True)
+def generate_qr(message):
+    qr_code = generate_qr_code(message.text, "qr_code.png")
+    bot.send_photo(message.chat.id, open(qr_code, "rb"))
 
 bot.infinity_polling()
